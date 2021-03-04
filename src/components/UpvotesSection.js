e32208fb2f4e7c66ceeab4d84e8868a2603f153b
Adam Mawlawi <adam@mradam.us>
2021-03-04 05:42:51 -0600

    fetching data from my back-end API

---
+++ src/components/UpvotesSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpvotesSection = () => {
+  return (
+    <div>
+      <button>Add vote</button>
+    </div>
+  );
+};
+
+export default UpvotesSection;
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articaleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
+import CommentsList from '../components/CommentsList';
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articaleContent.find((article) => article.name === name);
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
+
   if (!article) return <NotFoundPage />;
   const otherArt = articaleContent.filter((art) => art.name !== name);
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes}</p>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
-      <hr />
+
+      <CommentsList comments={articleInfo.comments} />
+
       <h3>Other Articles:</h3>
       <ArticlesList articles={otherArt} />
     </>
